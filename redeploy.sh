4fe26ae7363578c0112d5f68bf7bcd52b0704299
carghai <carghai74@gmail.com>
2023-07-06 17:20:00 -0400

    update

--- redeploy.sh
+++ redeploy.sh
@@ -9,6 +9,6 @@ tmux send-keys 'source python3-virtualenv/bin/activate' C-m
 
 tmux send-keys 'pip install -r requirements.txt' C-m
 
-tmux send-keys 'flask run' C-m
+tmux send-keys 'flask run --host=0.0.0.0' C-m
 
 echo "website up and running"
