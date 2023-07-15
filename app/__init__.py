b51aabcce2820d8e0724ed1bb06dd376bee1d6e4
carghai <carghai74@gmail.com>
2023-07-14 22:13:20 -0400

    work

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import *
 
 load_dotenv()
 app = Flask(__name__)
 
+mydb = MySQLDatabase(os.getenv("MYSQl_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+
+print(mydb)
 
 @app.route('/')
 def index():
@@ -29,3 +38,7 @@ def education():
 @app.route('/projects')
 def projects():
     return render_template('projects.html', title="Projects", url=os.getenv("URL"))
+
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', title="Timeline", url=os.getenv("URL"))
