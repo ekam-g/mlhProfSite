f6c800daa8c8db22364ffa0a74ef36854056c78d
carghai <carghai74@gmail.com>
2023-07-15 00:34:57 -0400

    stuff

--- app/__init__.py
+++ app/__init__.py
@@ -42,3 +42,33 @@ def projects():
 @app.route('/timeline')
 def timeline():
     return render_template('timeline.html', title="Timeline", url=os.getenv("URL"))
+# Model
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DataTimeField(default=datetime.datatime.now
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+#api
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name,email=email,content=content)
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', mothods=['GET'])
+def get_time_line_post():
+    return (
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in 
+TimelinePost.select().order_by(TimelinePost.created_at_desc())
+        ]
+    )
