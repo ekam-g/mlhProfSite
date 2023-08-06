b35fd927759f5f6069f9c59827f3e1b8336c068e
carghai <carghai74@gmail.com>
2023-08-05 21:01:42 -0400

    docker

--- redeploy.sh
+++ redeploy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 git fetch && git reset origin/main --hard
 
+git pull
+
 source python3-virtualenv/bin/activate
 
 pip install -r requirements.txt
 
-systemctl restart myportfolio
+# shellcheck disable=SC2046
+docker stop $(docker ps -a -q)
+
+# shellcheck disable=SC2046
+docker rm $(docker ps -a -q)
+
+docker compose -f docker-compose.prod.yml down
+
+docker compose -f docker-compose.prod.yml up -d --build
